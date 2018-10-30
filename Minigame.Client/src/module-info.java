5eda84ca9f8f3a67da4eda2587c1a45a0fcd9558
LakkieRicardo <lakkiegaming@gmail.com>
2018-10-30 09:40:27 -0700

    Switched to Java 8

--- Minigame.Client/src/module-info.java
+++ Minigame.Client/src/module-info.java
@@ -1,12 +1,12 @@
-/**
- * @author Lakkie
- *
- */
-module minigame.client {
-	
-	requires java.desktop;
-	requires java.logging;
-	
-	exports net.lakkie.minigame.client;
-	
-}
+///**
+// * @author Lakkie
+// *
+// */
+//module minigame.client {
+//	
+//	requires java.desktop;
+//	requires java.logging;
+//	
+//	exports net.lakkie.minigame.client;
+//	
+//}
--- Minigame.Test/src/module-info.java
+++ Minigame.Test/src/module-info.java
@@ -1,10 +1,10 @@
-/**
- * @author Lakkie
- *
- */
-module minigame.test {
-	
-	requires minigame.client;
-	requires minigame.server;
-	
-}
+///**
+// * @author Lakkie
+// *
+// */
+//module minigame.test {
+//	
+//	requires minigame.client;
+//	requires minigame.server;
+//	
+//}
